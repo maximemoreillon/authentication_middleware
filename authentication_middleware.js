cd30b646a01bac8ec27b02e79e71b078550205d7
Maxime Moreillon <m.moreillon@gmail.com>
2020-04-18 00:24:54 +0000

    added cookie verification

--- authentication_middleware.js
+++ authentication_middleware.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
+const Cookies = require('cookies')
 
 // secret set by the application
 exports.authentication_api_url = undefined
@@ -11,15 +12,28 @@ exports.middleware = (req, res, next) => {
     return res.status(500).send('Authentication API URL not set in the server-side application')
   }
 
-  // Check if authorization header is set
-  if(!req.headers.authorization) return res.status(403).send('Authorization header needs to be set for this route')
+  let jwt = undefined
 
-  // parse the headers to get the token
-  let token = req.headers.authorization.split(" ")[1];
-  if(!token) return res.status(403).send('JWT not present in authorization header')
+  // See if jwt available from authorization header
+  if(!jwt){
+    if(('authorization' in req.headers)) {
+      jwt = req.headers.authorization.split(" ")[1]
+    }
+  }
+
+  // Try to get JWT from cookies
+  if(!jwt) {
+    var cookies = new Cookies(req, res)
+    jwt = cookies.get('jwt')
+  }
+
+  // if no JWT available, reject request
+  if(!jwt) {
+    res.status(403).send('JWT not found in either cookies or authorization header')
+  }
 
   // Send the token to the authentication api for verification
-  axios.post(exports.authentication_api_url, {jwt: token})
+  axios.post(`${exports.authentication_api_url}`, {jwt: jwt})
   .then(response => {
 
     // passing the user object to the route using res.locals
