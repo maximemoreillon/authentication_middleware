c1648d14e0905b0afe3c092534bd6dc88cdda29b
Maxime Moreillon <m.moreillon@gmail.com>
2020-08-02 06:27:59 +0000

    aliases for env var

--- authentication_middleware.js
+++ authentication_middleware.js
@@ -1,25 +1,16 @@
 const axios = require('axios')
 const Cookies = require('cookies')
-const dotenv = require('dotenv');
+const dotenv = require('dotenv')
 
-dotenv.config();
+dotenv.config()
 
-// secret set by the application
 exports.authentication_api_url = process.env.AUTHENTICATION_API_URL
-exports.authentication_api_endpoint = '/user_from_jwt'
+  || process.env.AUTHENTICATION_MANAGER_URL
+  || process.env.AUTHENTICATION_MANAGER_API_URL
 
-exports.authenticate = (req, res, next) => {
+exports.authentication_api_route = '/user_from_jwt'
 
-  // Retrieves user information using the JWT provided by the user
-  // will NOT allow the user to proceed with the request if not authenticated
-
-  // check if the API URL is defined
-  // could just let the axios request fail
-  if(!exports.authentication_api_url){
-    console.log(new Error("Authentication API URL not set"))
-    return res.status(500).send('Authentication API URL not set in the server-side application')
-  }
-
+let retrieve_jwt = (req, res) => {
   let jwt = undefined
 
   // See if jwt available from authorization header
@@ -31,9 +22,27 @@ exports.authenticate = (req, res, next) => {
 
   // Try to get JWT from cookies
   if(!jwt) {
-    var cookies = new Cookies(req, res)
+    let cookies = new Cookies(req, res)
     jwt = cookies.get('jwt')
   }
+
+  return jwt
+}
+
+exports.authenticate = (req, res, next) => {
+
+  // Retrieves user information using the JWT provided by the user
+  // will NOT allow the user to proceed with the request if not authenticated
+
+  // check if the API URL is defined
+  // could just let the axios request fail
+  if(!exports.authentication_api_url){
+    console.error("Authentication API URL not set")
+    return res.status(500).send('Authentication API URL not set in the server-side application')
+  }
+
+  // retrieve JWT
+  let jwt = retrieve_jwt(req, res)
 
   // if no JWT available, reject request
   if(!jwt) {
@@ -45,7 +54,7 @@ exports.authenticate = (req, res, next) => {
   // Send the token to the authentication api for verification
   // IDEA: first request to AMS for content of JWT and second to UMMS for user information
   axios.get(
-    `${exports.authentication_api_url}${exports.authentication_api_endpoint}`,
+    `${exports.authentication_api_url}${exports.authentication_api_route}`,
     {params: {jwt: jwt}}
   )
   .then(response => {
@@ -77,20 +86,8 @@ exports.identify_if_possible = (req, res, next) => {
     return next()
   }
 
-  let jwt = undefined
-
-  // See if jwt available from authorization header
-  if(!jwt){
-    if(('authorization' in req.headers)) {
-      jwt = req.headers.authorization.split(" ")[1]
-    }
-  }
-
-  // Try to get JWT from cookies
-  if(!jwt) {
-    var cookies = new Cookies(req, res)
-    jwt = cookies.get('jwt')
-  }
+  // retrieve JWT
+  let jwt = retrieve_jwt(req, res)
 
   // if no JWT available, just proceed with request
   if(!jwt) {
@@ -98,57 +95,14 @@ exports.identify_if_possible = (req, res, next) => {
   }
 
   // Send the token to the authentication api for verification
-  // TODO: first request to AMS for content of JWT and second to UMMS for user information
-  axios.get(`${exports.authentication_api_url}${exports.authentication_api_endpoint}`, {params: {jwt: jwt}} )
+  axios.get(`${exports.authentication_api_url}${exports.authentication_api_route}`, {params: {jwt: jwt}} )
   .then(response => {
-
     // passing the user object to the route using res.locals
     res.locals.user = response.data
-
   })
-  .catch( () => { })
-  .finally( () => { next() })
-}
-
-exports.middleware = (req, res, next) => {
-  // This is now becoming legacy
-
-  // check if the API URL is defined
-  if(!exports.authentication_api_url){
-    console.log(new Error("Authentication API URL not set"))
-    return res.status(500).send('Authentication API URL not set in the server-side application')
-  }
-
-  let jwt = undefined
-
-  // See if jwt available from authorization header
-  if(!jwt){
-    if(('authorization' in req.headers)) {
-      jwt = req.headers.authorization.split(" ")[1]
-    }
-  }
-
-  // Try to get JWT from cookies
-  if(!jwt) {
-    var cookies = new Cookies(req, res)
-    jwt = cookies.get('jwt')
-  }
-
-  // if no JWT available, reject request
-  if(!jwt) {
-    return res.status(403).send('JWT not found in either cookies or authorization header')
-  }
-
-  // Send the token to the authentication api for verification
-  axios.post(`${exports.authentication_api_url}`, {jwt: jwt})
-  .then(response => {
-
-    // passing the user object to the route using res.locals
-    res.locals.user = response.data
-
-    // allow continuation to the route
+  .catch(error => { console.log(error) })
+  .finally( () => {
+    // No matter the outcome, allow to proceed
     next()
   })
-  .catch(error => { res.status(403).send(error) })
-
 }
