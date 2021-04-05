228912323a254c6a2ba49cb664432bc49bc9ff13
Maxime Moreillon <m.moreillon@gmail.com>
2021-04-05 03:59:52 +0000

    fixed axios vunlerability

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
 # Express authorization middleware
 
+A simple middleware for express that retrieves a JWT from a user's request and sends it to the authentication service for decoding.
+The authentication service then replies with the user information correspoding to the token, which the middleware saves as res.locals.user
+
 ## Usage
 
 ```
-const express = require('express')
+// Crerate an express app
+const app = require('express')()
+
+// Import que authentication middleware
 const auth = require('@moreillon/authentication_middleware')
 
+// Set the URL of the authentication service
+// Defaults to http://authentication
+// Can also be set using
 auth.authentication_api_url = 'https://auth.example.com'
-auth.authentication_api_route  = '/user_from_jwt'
 
-const app = express()
+// Register the middleware application-wide
 app.use(auth.authenticate)
 ```
--- authentication_middleware.js
+++ authentication_middleware.js
@@ -7,10 +7,15 @@ dotenv.config()
 exports.authentication_api_url = process.env.AUTHENTICATION_API_URL
   || process.env.AUTHENTICATION_MANAGER_URL
   || process.env.AUTHENTICATION_MANAGER_API_URL
+  || 'http://authentication'
 
 exports.authentication_api_route = '/user_from_jwt'
 
 let retrieve_jwt = (req, res) => {
+  /*
+  Retrieving the token from either cookies or authorization header
+  */
+
   let jwt = undefined
 
   // See if jwt available from authorization header
@@ -34,29 +39,19 @@ exports.authenticate = (req, res, next) => {
   // Retrieves user information using the JWT provided by the user
   // will NOT allow the user to proceed with the request if not authenticated
 
-  // check if the API URL is defined
-  // could just let the axios request fail
-  if(!exports.authentication_api_url){
-    console.error("Authentication API URL not set")
-    return res.status(500).send('Authentication API URL not set in the server-side application')
-  }
-
   // retrieve JWT
   let jwt = retrieve_jwt(req, res)
 
   // if no JWT available, reject request
   if(!jwt) {
-    console.log('JWT not found in either cookies or authorization header')
+    console.log('[Auth middleware] JWT not found in either cookies or authorization header')
     res.status(403).send('JWT not found in either cookies or authorization header')
     return
   }
 
   // Send the token to the authentication api for verification
-  // IDEA: first request to AMS for content of JWT and second to UMMS for user information
-  axios.get(
-    `${exports.authentication_api_url}${exports.authentication_api_route}`,
-    {params: {jwt: jwt}}
-  )
+  const url = `${exports.authentication_api_url}${exports.authentication_api_route}`
+  axios.get(url, {params: {jwt: jwt}} )
   .then(response => {
 
     // passing the user object to the route using res.locals
@@ -65,11 +60,10 @@ exports.authenticate = (req, res, next) => {
     // allow to request to proceed
     next()
   })
-  .catch(error => {
+  .catch( error => {
     // In case the request fails, forward error message
     console.log(error)
     res.status(403).send(error)
-    return
   })
 
 }
@@ -78,13 +72,6 @@ exports.identify_if_possible = (req, res, next) => {
 
   // Retrieves user information using the JWT provided by the user
   // WILL allow the user to proceed with the request if authentication fails
-
-  // check if the API URL is defined
-  // could just let the axios request fail
-  if(!exports.authentication_api_url){
-    console.log(new Error("Authentication API URL not set"))
-    return next()
-  }
 
   // retrieve JWT
   let jwt = retrieve_jwt(req, res)
